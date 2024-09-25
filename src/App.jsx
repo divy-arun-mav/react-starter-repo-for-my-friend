b2a570cc09fec2a58b4f5e792ba2472ae14ae987
Divy Mav <divymav5@gmail.com>
2024-09-25 23:16:39 +0530

    Le bhai lele isse ter code work karega

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Routes } from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+      <strong className='text-center text-3xl'>Idar tera code likh</strong>
+      <Routes>
+        {/* Add your route paths and components here */}
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+    </div>
+  );
+}
+export default App;
